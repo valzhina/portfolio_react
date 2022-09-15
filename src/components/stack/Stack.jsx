e2cc9be99c98e4f76da2446900ee5d118b47d217
Valzhina <valzhinapiskun@gmail.com>
2022-09-15 00:15:51 -0700

    sccs updste

--- src/components/stack/Stack.jsx
+++ src/components/stack/Stack.jsx
@@ -3,7 +3,26 @@ import "./stack.scss"
 export default function Stack() {
     return (
         <div className='stack' id='stack'>
-            stack
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/arrow-down-3101.png" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>
+                                    Lpoiuiu ytryre piuyu ,mnetn poiueptr.
+                                    D'liopioty ytryre piuyu opiuytmv bcklm.  
+                                </p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
